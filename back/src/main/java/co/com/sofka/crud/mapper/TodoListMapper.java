eb3e86cac49a2dd36968fd4a7b7be8c60bf4815e
sebastian <sebas99cano@gmail.com>
2021-06-03 20:45:20 -0500

    se creo la clase TodoListDto y la clase TodoListMapper

---
+++ back/src/main/java/co/com/sofka/crud/mapper/TodoListMapper.java
@@ -0,0 +1,22 @@
+package co.com.sofka.crud.mapper;
+
+import co.com.sofka.crud.dto.TodoListDto;
+import co.com.sofka.crud.entity.TodoList;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface TodoListMapper {
+
+    @Mappings({
+            @Mapping(source = "id", target = "id"),
+            @Mapping(source = "name", target = "name"),
+    })
+    TodoListDto toTodoListDto(TodoList todoList);
+    Iterable<TodoListDto> toTodoListDto (Iterable<TodoList> todoList);
+
+    @InheritInverseConfiguration
+    TodoList toTodoList(TodoListDto todoDto);
+}
